cff25ff11c635b23ecb76171a8d720fbc23a9e3b
deni3278 <deni3278@easv365.dk>
2021-05-06 16:28:22 +0200

    Update modality of Delete Reservation

--- src/main/java/camper/controller/ReservationsController.java
+++ src/main/java/camper/controller/ReservationsController.java
@@ -4,9 +4,11 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.DialogPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -29,11 +31,22 @@ public class ReservationsController {
         paneNewReservation = FXMLLoader.load(getClass().getResource("../fxml/NewReservation.fxml"));
         paneDeleteReservation = FXMLLoader.load(getClass().getResource("../fxml/DeleteReservation.fxml"));
 
-        Stage popup = new Stage();
-        popup.setScene(new Scene(paneDeleteReservation));
+        Button btnCancel = (Button) paneDeleteReservation.lookupButton(ButtonType.CANCEL);
+        Button btnApply = (Button) paneDeleteReservation.lookupButton(ButtonType.APPLY);
 
         btnAllReservations.setOnAction(e -> root.setCenter(paneReservations));
         btnNewReservation.setOnAction(e -> root.setCenter(paneNewReservation));
-        btnDeleteReservation.setOnAction(e -> popup.show());
+        btnDeleteReservation.setOnAction(e -> {
+            Stage popup = new Stage();
+
+            popup.setTitle("Delete Reservation");
+            popup.initModality(Modality.APPLICATION_MODAL);
+            popup.setScene(new Scene(paneDeleteReservation));
+
+            btnCancel.setOnAction(event -> popup.close());
+            btnApply.setOnAction(event -> popup.close());
+
+            popup.show();
+        });
     }
 }
